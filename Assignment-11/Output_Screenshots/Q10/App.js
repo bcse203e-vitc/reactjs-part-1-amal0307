f26a0ce58d0555798b393a585ee0d117ef9fcba0
Amal M Raju <166976243+amal0307@users.noreply.github.com>
2025-02-25 16:22:29 +0530

    Create App.js

---
+++ Assignment-11/Output_Screenshots/Q10/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+function App() {
+  return(
+     <img src="https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg" alt='photo'/>
+  )
+
+}
+
+export default App;
